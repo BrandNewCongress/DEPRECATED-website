131e8b74344f82b66c0ff5e244a4b3d70dc91817
saikat <saikat@gomockingbird.com>
2016-05-18 11:32:09 -0400

    Ugly header

--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -7,12 +7,13 @@ import { StyleSheet } from 'react-look'
 const styles = StyleSheet.create({
   map: {
     width: '100%',
-    height: '100vh'
+    height: '80vh'
   }
 })
 
 export default () => (
   <div className={styles.map}>
+    <Navigation />
     <OpenStreetMap />
   </div>
 )
--- src/frontend/components/Navigation.js
+++ src/frontend/components/Navigation.js
@@ -1,18 +1,50 @@
 import React from 'react'
+import { StyleSheet } from 'react-look'
+
+const styles = StyleSheet.create({
+  logo: {
+    backgroundColor: '#f16432',
+    padding: '10px 5px 5px',
+    width: 100,
+    marginTop: 5,
+    float: 'left'
+  },
+  logoText: {
+    color: 'white',
+    padding: '5px 5px 5px 10px',
+    marginLeft: 10,
+    border: 'solid white',
+    borderWidth: '0 0 0 4px',
+    marginTop: 5,
+    marginBottom: 5,
+    fontSize: 15
+  },
+  navBar: {
+    float: 'left'
+  }
+})
 
 export default () => (
-  <div className='container'>
-    <nav role='navigation'>
-      <div id='logo' className='pull-left' style={{ float: 'left' }}>
-        <h3>Brand<br />New<br />Congress</h3>
+  <div>
+    <div className={styles.logo}>
+      <div className={styles.logoText}>
+        Brand New Congress
       </div>
-      <ul className='nav navbar-nav'>
-        <li role='presentation'><a href='#'>The Tour</a></li>
-        <li role='presentation'><a href='#'>The Plan</a></li>
-        <li role='presentation'><a href='#'>Who's Involved</a></li>
-        <li role='presentation'><a href='#'>Join Us</a></li>
-        <li role='presentation'><a href='#'>Contribute</a></li>
-      </ul>
-    </nav>
+    </div>
+    <div>
+      <div>
+        We're coming to a city near you
+      </div>
+      <div>
+        RSVP below, and let's get together to elect a Brand New Congress that works for all. Can't make it? Get involved anyway.
+      </div>
+    </div>
+    <div className={styles.navBar}>
+      <a href='#'>The Tour</a>
+      <a href='#'>The Plan</a>
+      <a href='#'>Who's Involved</a>
+      <a href='#'>Join A Team</a>
+      <a href='#'>Contribute</a>
+    </div>
   </div>
 )
