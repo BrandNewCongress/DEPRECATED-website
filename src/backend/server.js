b34c6fa13da4a45dc3a7370586e2a8446798ecfd
saikat <saikat@gomockingbird.com>
2016-05-19 19:10:17 -0400

    Put in proxy to RP to nationbuilder

--- src/backend/server.js
+++ src/backend/server.js
@@ -3,6 +3,7 @@ import express from 'express'
 import fallback from 'express-history-api-fallback'
 import path from 'path'
 import log from './log'
+import proxy from 'http-proxy-middleware'
 
 // Properly catch async exceptions, log them, and re-throw them
 // on the main process to crash the program
@@ -16,22 +17,23 @@ const wrap = (fn) =>
 const app = express()
 const port = process.env.PORT
 const publicPath = path.resolve(__dirname, '../../build/frontend')
-
-// This serves index.html
-app.use(express.static(publicPath, {
-  maxAge: '180 days'
-}))
-
-app.get('/hello-world', wrap((req, res) => {
-  log.info('Hello World!')
-  res.send('Hello World!')
-}))
+app.enable('trust proxy')
 
 app.listen(port, () => {
   log.info(`Node app is running on port ${port}`)
 })
 
-// This lets us use client-side routing for any routes not defined in server.js
+app.get('/', wrap((req, res) => {
+  res.redirect('/home')
+}))
+
+app.use(express.static(publicPath, {
+  maxAge: '180 days'
+}))
+
+app.use(['/home', '/teams', '/faq'],
+  proxy({ target: 'http://brandnewcongress.nationbuilder.com/', changeOrigin: true }))
+
 app.use(fallback('index.html', {
   root: publicPath,
   maxAge: 0
