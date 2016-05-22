6e0c50e45c3749fa3f0e563c16854ecd18e32ea1
Rapi Castillo <rapi.castillo@gmail.com>
2016-05-22 11:33:02 -0700

    Navigation fixes for Issue #18

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { StyleSheet } from 'react-look'
 import theme from '../theme'
 
+const c = StyleSheet.combineStyles
 const styles = StyleSheet.create({
   logo: {
     display: 'inline-block',
@@ -24,17 +25,38 @@ const styles = StyleSheet.create({
     '@media (min-width: 575px)': {
       display: 'none'
     },
+    display: 'inline-block',
     backgroundColor: theme.colors.orange,
-    display: 'flex',
-    flex: 1,
-    marginLeft: 1
+    marginLeft: 1,
+    float: 'right',
+    paddingRight: 18,
+    paddingTop: 18
+  },
+  mobileNavList: {
+    '@media (min-width: 575px)': {
+      display: 'none'
+    },
+    height: 0,
+    transition: 'height 0.2s',
+    overflow: 'hidden',
+    width: '100%'
+  },
+  showNav: {
+    height: 115
+  },
+  hideNav: {
+    height: 0
+  },
+  navArea: {
+    display: 'block'
   },
   nav: {
     '@media (max-width: 575px)': {
       display: 'none'
     },
-    display: 'flex',
-    flex: 1,
+    display: 'inline-block',
+    // display: 'flex',
+    // flex: 1,
     fontWeight: 400,
     fontSize: 15,
     color: theme.colors.orange,
@@ -43,37 +65,94 @@ const styles = StyleSheet.create({
   },
   navItem: {
     display: 'inline-block',
-    paddingRight: 25
+    paddingRight: 25,
+    '@media (max-width: 575px)': {
+      display: 'block',
+      paddingLeft: 9,
+      color: 'white',
+      paddingTop: 2,
+      paddingBottom: 4,
+      borderLeft: '4px white solid',
+      marginLeft: 15,
+      fontSize: 14,
+      marginTop: 4
+    }
   },
   container: {
     display: 'flex',
-    width: '100%'
+    width: '100%',
+    '@media (max-width: 575px)': {
+      backgroundColor: theme.colors.orange,
+      flexDirection: 'column'
+    }
+  },
+  viewport: {
+    clear: 'both',
+    width: '100%',
+    display: 'block'
   }
 })
 
-export default () => (
-  <div className={styles.container}>
-    <div className={styles.logo}>
-      <div className={styles.logoText}>
-        Brand New Congress
-      </div>
+const navigationLinks = (
+  <div className={styles.navArea}>
+    <div className={styles.navItem}>
+    The Plan
     </div>
-    <div className={styles.mobileNav}>
-      PUT MOBILE NAV HERE
+    <div className={styles.navItem}>
+    Who's Involved
     </div>
-    <div className={styles.nav}>
-      <div className={styles.navItem}>
-      The Plan
-      </div>
-      <div className={styles.navItem}>
-      Who's Involved
-      </div>
-      <div className={styles.navItem}>
-      Join A Team
-      </div>
-      <div className={styles.navItem}>
-      Contribute
-      </div>
+    <div className={styles.navItem}>
+    Join A Team
+    </div>
+    <div className={styles.navItem}>
+    Contribute
     </div>
   </div>
 )
+
+export default class Navigation extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      showNav: false
+    }
+
+    this.showMobileNav = this.showMobileNav.bind(this)
+  }
+
+  showMobileNav() {
+    const showMobile = !this.state.showNav
+    this.setState({ showNav: showMobile })
+  }
+
+  render() {
+
+    return(
+      <div className={styles.container}>
+        <div className={styles.viewport}>
+          <div className={styles.logo}>
+            <div className={styles.logoText}>
+              Brand New Congress
+            </div>
+          </div>
+          <div
+            className={styles.mobileNav}
+            onClick={() => this.showMobileNav()}
+            onTouch={() => this.showMobileNav()}
+          >
+            <img src={'http://placehold.it/30x30'} role='presentation' />
+          </div>
+          <div className={styles.nav}>
+            {navigationLinks}
+          </div>
+        </div>
+        <div style={ {clear: 'both'} }></div>
+        <div
+          className={ this.state.showNav ? c(styles.mobileNavList, styles.showNav) : styles.mobileNavList }>
+          { navigationLinks }
+        </div>
+      </div>
+    )
+  }
+}
