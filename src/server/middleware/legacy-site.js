9d83e059c3f275808441484db55db53260cad2f6
ryankopf <ryan@ryankopf.com>
2016-06-23 10:57:09 -0500

    Enable Markdown pages in the static-site directory.

--- src/server/middleware/legacy-site.js
+++ src/server/middleware/legacy-site.js
@@ -7,26 +7,38 @@
 import fs from 'fs'
 import path from 'path'
 
-//export const SITE_DIR = path.resolve(__dirname, '..', '..', 'src/static-site')
 export const SITE_DIR = path.resolve(process.cwd(), 'src/static-site')
 export default function (req, res, next) {
   const { name: pathname } = path.parse(req.originalUrl)
   const possibleStaticPath = path.join(SITE_DIR, `${pathname}.html`)
   fs.stat(possibleStaticPath, err => {
     const fileExists = !err
-    if (fileExists) {
-      //Simplify design on the legacy pages by having a unified header and footer.
+    if (fileExists) { //Simplify design on the legacy pages by having a unified header and footer.
       var header_path = path.join(SITE_DIR, `header.html`)
       var footer_path = path.join(SITE_DIR, `footer.html`)
-      var header = fs.readFileSync(header_path, 'utf8');
+      var header = fs.readFileSync(header_path, 'utf8')
+      var footer = fs.readFileSync(footer_path, 'utf8')
       var page = fs.readFileSync(possibleStaticPath, 'utf8');
-      var footer = fs.readFileSync(footer_path, 'utf8');
       if (pathname == "home") { var header = ""; var footer = "";} // No custom header or footer for the homepage
       res.send(header + page + footer);
-      //res.sendFile(possibleStaticPath)
     } else {
-      // We simply continue and allow express's default 404 handler to kick in.
-      next()
+      const possibleStaticPath_M = path.join(SITE_DIR, `${pathname}.markdown`)
+      fs.stat(possibleStaticPath_M, err => {
+        const fileExists2 = !err
+        if (fileExists2) {
+          var header_path = path.join(SITE_DIR, `header.html`)
+          var footer_path = path.join(SITE_DIR, `footer.html`)
+          var header = fs.readFileSync(header_path, 'utf8')
+          var footer = fs.readFileSync(footer_path, 'utf8')
+          var page = fs.readFileSync(possibleStaticPath_M, 'utf8');
+          var markdown = require( "markdown" ).markdown;
+          if (pathname == "home") { var header = ""; var footer = "";} // No custom header or footer for the homepage
+          res.send(header + "<div class='markdown_content'>" + markdown.toHTML( page ) + "</div>" + footer);
+        } else {
+          // We simply continue and allow express's default 404 handler to kick in.
+          next()
+        }
+      })
     }
   })
 }
---
+++ src/static-site/eventsteam.markdown
@@ -0,0 +1,22 @@
+Events Team
+============
+
+Welcome to the Brand New Congress Events Team page!
+------------
+
+BNC Teams are joined by applying. We're looking for team players who work really well with others and have significant time each week to contribute. We are not on boarding new members right at this moment, but please apply if you want to get involved because we will begin growing our teams again soon!
+
+Our team is working with local volunteers around the country to organize our 100-city tour, by connecting with local Bernie grassroots leaders to find locations (usually just homes) and do publicity.
+
+If you'd like to join the events team, follow the steps below:
+
+1. Read all the information on the [front page of the site](/) and read the [Frequently Asked Questions](/faq) page.
+
+2. Join a BNC orientation conference call. [The schedule is here.](/call)
+
+3. If, after learning more about BNC and hearing about other teams, you'd still like to join this team, then please send us some details about your city using [this form](https://docs.google.com/forms/d/1aViZvPENrXa4Uq-a0UtLU84V_tyxKS35pIAZ8wT98L0/viewform).  We'll get back to you as soon as we can!
+
+What does the Events team do?
+------------
+
+Members of the events team take responsibility for arranging tour stops for the BNC summer tour in one or more cities. You will reach out to grassroots leaders from the BNC list and local Facebook pages and groups of the Bernie and other movements to find hosts and venues for our tour stops. You will manage event listings on Facebook and our website, monitor and communicate with RSVPs, work with hosts to prepare them for their event. The team may develop specialized roles that only do one of these various functions, so don't hesitate to join if all that sounds a little intimidating.
