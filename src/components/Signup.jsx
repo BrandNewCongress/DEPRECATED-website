a8f79e74ae18ea52bd4ce38c6d5e224c9e09fb1d
saikat <saikat@gomockingbird.com>
2016-07-13 00:48:35 -0400

    Revert "Merge pull request #95 from nmbusman/form_above_fold"
    This reverts commit ce949552c16d3b30d5176142b830238484cc93c7, reversing
    changes made to ec17f38ae60924e2c844c98a044e0ae5f94e4af5.

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -25,32 +25,17 @@ const styles = StyleSheet.create({
     padding: '5px 5px 5px 5px',
     border: `1px solid ${theme.colors.lightGray}`
   },
-  explanationCongress: {
-  [onTablet]: {
-    order: 1
-    }
-  },
-  congressHeader: {
+  header: {
     ...theme.text.header,
     paddingBottom: 20,
-    color: theme.colors.orange,
+    color: theme.colors.orange
   },
   secondaryHeader: {
     ...theme.text.header,
     paddingBottom: 20,
-    color: theme.colors.purple,
-    [onTablet]: {
-      order: 2
-      }
+    color: theme.colors.purple
   },
-  congressBody: {
-    display: 'block',
-    paddingBottom: 30,
-    lineHeight: '1.5em',
-    [onTablet]: {
-      order: 4
-    }
-  },
+
   body: {
     display: 'block',
     paddingBottom: 30,
@@ -66,15 +51,13 @@ const styles = StyleSheet.create({
       marginRight: 25
     }
   },
-  explanationForm: {
+  form: {
     width: 350,
     marginRight: 30,
     [onTablet]: {
       width: '80%',
       marginRight: 'auto',
-      marginLeft: 'auto',
-      marginBottom: 20,
-      order: 3
+      marginLeft: 'auto'
     }
   },
   line: {
@@ -172,22 +155,20 @@ export default class Signup extends React.Component {
       <div className={styles.container}>
         <div className={styles.contentContainer}>
           <div className={styles.explanation}>
-            <div className={styles.explanationCongress}>
-              <div className={styles.congressHeader}>Congress is broken.
-              </div>
-              <div className={styles.body}>
-              <a target='_blank' className={styles.link} href='http://www.realclearpolitics.com/epolls/other/congressional_job_approval-903.html'>80% of Americans agree.</a> Its decisions are <a target='_blank' className={styles.link} href='http://www.bbc.com/news/blogs-echochambers-27074746'>driven by a handful of wealthy individuals</a>, it is incapable of working together to enact real change, and <a target='_blank' className={styles.link} href='https://www.youtube.com/watch?v=Ylomy1Aw9Hk'>its members spend too much time dialing for dollars</a>.
-              </div>
+            <div className={styles.header}>Congress is broken.
+            </div>
+            <div className={styles.body}>
+            <a target='_blank' className={styles.link} href='http://www.realclearpolitics.com/epolls/other/congressional_job_approval-903.html'>80% of Americans agree.</a> Its decisions are <a target='_blank' className={styles.link} href='http://www.bbc.com/news/blogs-echochambers-27074746'>driven by a handful of wealthy individuals</a>, it is incapable of working together to enact real change, and <a target='_blank' className={styles.link} href='https://www.youtube.com/watch?v=Ylomy1Aw9Hk'>its members spend too much time dialing for dollars</a>.
             </div>
             <div className={styles.secondaryHeader}>
             Let's fix it.
             </div>
-            <div className={styles.congressBody}>
+            <div className={styles.body}>
             We need <span className={styles.bold}>an honest, accountable Congress</span>, but trying to win each congressional seat one-by-one is impossible.  So let's replace Congress all at once. Our plan is to recruit and run 400+ candidates as a single, unified campaign with a single plan. By giving the people an option for big, tangible change, we plan to whip up the same enthusiasm, volunteerism, voter turnout, and grassroots donations as Bernie's presidential campaign. <span className={styles.leadIn}>Let's elect a Brand New Congress that works for the people.</span>
             </div>
-            <div className={styles.explanationForm}>
-              {this.renderForm()}
-            </div>
+          </div>
+          <div className={styles.form}>
+            {this.renderForm()}
           </div>
           <Snackbar
             open={this.state.error}
