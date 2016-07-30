170d7e4fdd680edc429b569c99834c8b8cb2824b
Jacob Rockland <Jake@Alexandras-MacBook-Air-4.local>
2016-07-30 00:50:27 -0700

    implemented abteam.md and callteam.md

--- src/server/index.js
+++ src/server/index.js
@@ -52,9 +52,7 @@ app.use('/images', express.static(
 app.use('/static-assets', express.static(path.resolve(STATIC_SITE_DIR, 'assets')))
 
 app.use([
-  '/callteam',
   '/about',
-  '/abteam',
   '/adteam',
   '/call',
   '/conferencecallteam',
