609f61bdd7fbd847957d66b5e4288780083d3958
Jeff Bellsey <jbellsey@gmail.com>
2016-11-27 17:32:27 -0800

    Update design of About page * Moves the page from static MD to dynamic components * Puts the team member data into a JS file for easy maintenance,    and to make it easy to create sub-groups and/or load the data    from a server

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,6 +2,7 @@ import { Route } from 'react-router'
 import Home from './components/Home'
 import Call from './components/Call'
 import App from './components/App'
+import About from './components/About'
 import React from 'react'
 import HostCall from './components/HostCall'
 import StaticSiteRenderer from './containers/StaticSiteRenderer'
@@ -9,10 +10,11 @@ import TrumpPetition from './components/TrumpPetition'
 
 export default (staticSite) => {
   let childRoutes = [
-    <Route path='home' component={Home} />,
-    <Route path='call' component={Call} />,
-    <Route path='hostcall' component={HostCall} />,
-    <Route path='petitions/trump-denounce-hate' component={TrumpPetition} />
+    <Route path='home'     key='home'  component={Home} />,
+    <Route path='about'    key='about' component={About} />,
+    <Route path='call'     key='call'  component={Call} />,
+    <Route path='hostcall' key='host'  component={HostCall} />,
+    <Route path='petitions/trump-denounce-hate' key='trump' component={TrumpPetition} />
   ]
 
   Object.keys(staticSite).forEach((route) => {
